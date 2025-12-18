081b4010ef80bbfd80faab83293b3901b0aa707b
Lakshay Nareda <naredalakshay@gmail.com>
2025-12-18 14:12:31 +0530

    chore: added comment

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+//routes1
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
